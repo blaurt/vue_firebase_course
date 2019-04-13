58acdea055a5d9ba73e8114c8e3244d267a8224f
SuverovDmitriy <suverov.dmitriy@gmail.com>
2019-04-14 00:58:34 +0300

    done part 12

---
+++ src/components/Ads/Ad.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <v-card>
+          <v-responsive>
+            <v-img
+              src="http://img10.joyreactor.cc/pics/post/Tofu93-furry-artist-furry-%D1%84%D1%8D%D0%BD%D0%B4%D0%BE%D0%BC%D1%8B-5131385.png"
+              height="300px"
+            />
+          </v-responsive>
+          <v-card-text>
+            <h1 class="text--primary">Lorem</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam non dolore consequuntur beatae pariatur aut sint obcaecati facilis veritatis consectetur nemo saepe fugiat repudiandae perferendis minus quasi, cumque unde iste!</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning" flat>Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ads: [
+        {
+          id: 1,
+          title: "1st",
+          description: "11111111",
+          promo: false,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,27 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Ad list</h1>
+      <v-flex xs12 md6 offset-sm3>
+        <h1 class="text--secondary mb-3">Ad list</h1>
+        <v-card v-for="ad in ads" class="elevation-10 mb-3" :key="ad.id">
+          <v-layout row wrap>
+            <v-flex xs4>
+              <v-responsive>
+                <img :src="ad.src" alt height="175px">
+              </v-responsive>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ad.title}}</h2>
+                <p>{{ad.description}}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="info" :to="`/ad/${ad.id}`">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +31,15 @@
 export default {
   data() {
     return {
-        
+      ads: [
+        {
+          id: 1,
+          title: "1st",
+          description: "11111111",
+          promo: false,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+        }
+      ]
     };
   }
 };
